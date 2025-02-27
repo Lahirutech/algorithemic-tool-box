aa83080602690186081a965a5da66ba6a81d70e5
Lanka R <rmllbrathnayaka@gmail.com>
2025-02-27 11:13:31 +1100

    fibbonaci with mode

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,4 +1,12 @@
+def get_pisano_period(m):
+    prev, curr = 0, 1
+    for i in range(0, m * m):  # Pisano period is at most m*m
+        prev, curr = curr, (prev + curr) % m
+        if prev == 0 and curr == 1:
+            return i + 1
+
 def fibonacci_huge_naive(n, m):
+    n=n%get_pisano_period(m)
     if n <= 1:
         return n
 
@@ -6,9 +14,9 @@ def fibonacci_huge_naive(n, m):
     current  = 1
 
     for _ in range(n - 1):
-        previous, current = current, previous + current
+        previous, current = current, (previous + current)%m
 
-    return current % m
+    return current if n else 0
 
 
 if __name__ == '__main__':
