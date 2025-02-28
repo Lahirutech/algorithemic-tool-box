355c1cab245d7726aa2107dc4f74b5924473cf2d
Lanka R <rmllbrathnayaka@gmail.com>
2025-02-28 12:52:02 +1100

    complete

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -1,19 +1,24 @@
 # Uses python3
 import sys
 
+def calculate_fib(n):
+    if n<=1:
+        return n
+
+    a,b=0,1
+    for _ in range (n-1):
+        a,b=b,a+b
+    return b%10
+
+
 def fibonacci_partial_sum_naive(from_, to):
-    _sum = 0
-
-    current = 0
-    _next  = 1
-
-    for i in range(to + 1):
-        if i >= from_:
-            _sum += current
-
-        current, _next = _next, current + _next
-
-    return _sum % 10
+    from_ =from_%60
+    to =to%60
+    if to < from_:  # Handle wrap-around case
+        to += 60
+    fibFrom=calculate_fib(from_+1)
+    toFib=calculate_fib(to+2)
+    return (toFib-fibFrom)%10
 
 
 if __name__ == '__main__':
--- week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
+++ week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
@@ -1,14 +1,18 @@
-def fibonacci_sum_squares(n):
-    if n <= 1:
+
+def calculate_fib(n):
+    if n<=1:
         return n
 
-    previous, current, sum = 0, 1, 1
+    a,b=0,1
+    for _ in range (n-1):
+        a,b=b,a+b
+    return b%10
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current * current
-
-    return sum % 10
+def fibonacci_sum_squares(n):
+    n=n%60
+    fibn=calculate_fib(n)
+    fibn2=calculate_fib(n+1)
+    return (fibn*fibn2)%10
 
 
 if __name__ == '__main__':
